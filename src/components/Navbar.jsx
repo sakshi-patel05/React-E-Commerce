6c1b70c2d8e6b06320f8d4cac501481d2f17ae10
sakshi-patel <sp7004675@gmail.com>
2026-07-13 19:01:53 +0530

    code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { CiMenuBurger } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
 
 
 const Navbar = () => {
-    const [menubar,setManBar]=useState(false)
+    const [menubar, setManBar] = useState(false)
     console.log(menubar);
-    
+
     return (
         <nav className='bg-white shadow-md'>
             <div className='max-w-7xl mx-auto px-5'>
@@ -21,17 +22,27 @@ const Navbar = () => {
                         <li className='cursor-pointer hover:text-blue-600'>Contact</li>
                     </ul>
 
-                    <button 
-                    className='md:hidden'
-                    onClick={()=>{
-                        setManBar(!menubar)
-                    }}
+                    <button
+                        className='md:hidden'
+                        onClick={() => {
+                            setManBar(!menubar)
+                        }}
                     >
-                        ()
-                        <CiMenuBurger />
+                        {(menubar) ? (<IoMdClose />) : (<CiMenuBurger />
+
+                        )}
                     </button>
 
                 </div>
+                {/* Mobile menu*/}
+                {menubar && (
+                    <ul className="md:hidden flex flex-col items-center gap-5 pb-6 font-medium bg-white w-full absolute left-0 shadow-lg transition-all duration-300">
+                        <li className="cursor-pointer hover:text-blue-600 w-full text-center py-2">HOME</li>
+                        <li className="cursor-pointer hover:text-blue-600 w-full text-center py-2">About</li>
+                        <li className="cursor-pointer hover:text-blue-600 w-full text-center py-2">Card</li>
+                        <li className="cursor-pointer hover:text-blue-600 w-full text-center py-2">Contact</li>
+                    </ul>
+                )}
 
             </div>
         </nav>
