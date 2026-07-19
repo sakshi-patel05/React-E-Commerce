387d2e0544679da798e8f8cea2777cc4e01474d0
sakshi-patel <sp7004675@gmail.com>
2026-07-19 15:18:16 +0530

    code

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+    <div>
+      <section className='max-w-7xl mx-auto px-5 mt-8'>
+        <div className='bg-[#111827] rounded-3xl overflow-hidden'></div>
+      </section>
+    </div>
+    </>
+  )
+}
+
+export default Hero
+
