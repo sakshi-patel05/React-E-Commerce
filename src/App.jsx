55fd4154d937a778b0cd0e9af9d17573af2945fa
sakshi-patel <sp7004675@gmail.com>
2026-07-22 15:18:44 +0530

    SearchCategory -Heading section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import SearchCategory from './components/SearchCategory'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero />
+      <SearchCategory />
     </div>
   )
 }
---
+++ src/components/SearchCategory.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SearchCategory = () => {
+  return (
+    <div>
+      <section className='max-w-7xl ma-auto px-5 py-10'>
+        {/* Heading */}
+        <h2 className='text-3xl font-bold md-8'>
+            Search By Category
+        </h2>
+      </section>
+    </div>
+  )
+}
+
+export default SearchCategory
